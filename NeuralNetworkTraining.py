02b1309d6e2b8d17924a97cc8ea3e4cbe528e897
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-05-20 00:57:40 +0200

    I think the problem with my neural network is the input data.

--- NeuralNetworkTraining.py
+++ NeuralNetworkTraining.py
@@ -1,7 +1,18 @@
 from keras.datasets import mnist
 import numpy as np
 import SimpleNeuralNetwork as snn
-import modules.NeuralNetworksAndDeepLearning.src.network as nn
+import packages.NeuralNetworkPython3.chapter1_2.Network as nn
+
+
+def convert_array(array: np.array) -> np.array:
+    """
+    Convert a 1D numpy array into a 2D matrix with one column.
+
+    :param array: 1D numpy array
+    :return: 2D numpy column array with the same entry.
+    """
+    return np.reshape(array, (len(array), 1))
+
 
 num_pixels = 784  # Number of pixels in a training images.
 num_output_neurons = 10  # Number of neurons in the output layer.
@@ -10,7 +21,7 @@ layer_sizes = np.array([784, 10])  # Sizes of the layers in the neural network.
 weights = [np.random.rand(num_output_neurons, num_pixels)]  # Weights used in the neural network.
 biases = [np.random.rand(num_output_neurons)]  # Biases used in the neural networks.
 
-print(weights, biases)
+# print(weights, biases)
 
 neural_network = snn.SimpleNeuralNetwork(layer_sizes, weights, biases)  # Define the neural network.
 (train_inputs, desired_results), (test_X, test_y) = mnist.load_data()  # Load the test data
@@ -36,15 +47,15 @@ converted_desired_results = np.array([convert_number(num) for num in desired_res
 # train_inputs = np.ndarray([data.reshape(num_pixels) for data in train_inputs])
 train_inputs = train_inputs.reshape(len(train_inputs), num_pixels)
 training_data = list(zip(train_inputs, converted_desired_results))
-mini_batch_size = 1000
+mini_batch_size = 10
 learning_rate = 3.
-epochs = 10
+epochs = 3
 
 print("Started learning.")
 neural_network.learn(training_data, mini_batch_size, epochs, learning_rate)  # Let the neural network learn.
 print("Finished learning.")
 
-print(neural_network.weights, neural_network.biases)
+# print(neural_network.weights, neural_network.biases)
 
 # Test the neural network by going through the test images and counting the number of rightly classified images.
 result_counter = 0
@@ -61,5 +72,10 @@ for data, desired_result in verification_data:
 
 print(f"{result_counter} of {len(test_X)} test images were verified correctly by my net work.")
 
-reference_neural_network = nn.Network(layer_sizes, weights, biases)
-reference_neural_network.SGD(training_data, epochs, mini_batch_size, learning_rate, test_data=verification_data)
+reference_neural_network = nn.Network(layer_sizes, weights, [convert_array(bias_vec) for bias_vec in biases])
+
+# Convert format of the data.
+training_data = [(convert_array(x), convert_array(y)) for x, y in training_data]
+verification_data = [(convert_array(x), y) for x, y in verification_data]
+
+reference_neural_network.learn(training_data, epochs, mini_batch_size, learning_rate, test_data=verification_data)
