9fe308bc80325f117ffc1c4539d503b17a33964c
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-05-20 01:49:11 +0200

    Moved the loading of the MNIST data to a seperate file.

--- NeuralNetworkTraining.py
+++ NeuralNetworkTraining.py
@@ -1,42 +1,18 @@
-from keras.datasets import mnist
 import numpy as np
 import SimpleNeuralNetwork as snn
 import packages.NeuralNetworkPython3.chapter1_2.Network as nn
+import loadMnistData as lmd
 
+num_pixels, training_data, verification_data = lmd.load_data()
 
-num_pixels = 784  # Number of pixels in a training images.
 num_output_neurons = 10  # Number of neurons in the output layer.
 layer_sizes = np.array([784, 10])  # Sizes of the layers in the neural network.
 
 weights = [np.random.rand(num_output_neurons, num_pixels)]  # Weights used in the neural network.
 biases = [np.random.rand(num_output_neurons)]  # Biases used in the neural networks.
 
-# print(weights, biases)
-
 neural_network = snn.SimpleNeuralNetwork(layer_sizes, weights, biases)  # Define the neural network.
-(train_inputs, desired_results), (test_X, test_y) = mnist.load_data()  # Load the test data
 
-
-def convert_number(number: int) -> np.ndarray:
-    """
-    This function converts a number of the training data of the expected results to a numpy array with a one at the
-    index num.
-
-    :return: Numpy array with a 1 at the index num.
-    """
-    vec = np.zeros(10)
-    vec[number] = 1.
-    return vec
-
-
-# Convert the elements of the desired results from numbers to numpy arrays.
-converted_desired_results = np.array([convert_number(num) for num in desired_results])
-
-# Declare the parameters for the neural network.
-# Reshape the training input so that it is only one vector.
-# train_inputs = np.ndarray([data.reshape(num_pixels) for data in train_inputs])
-train_inputs = train_inputs.reshape(len(train_inputs), num_pixels)
-training_data = list(zip(train_inputs, converted_desired_results))
 mini_batch_size = 10
 learning_rate = 3.
 epochs = 3
@@ -49,9 +25,6 @@ print("Finished learning.")
 
 # Test the neural network by going through the test images and counting the number of rightly classified images.
 result_counter = 0
-test_X = test_X.reshape(len(test_X), num_pixels)
-
-verification_data = list(zip(test_X, test_y))
 
 for data, desired_result in verification_data:
 
@@ -60,7 +33,9 @@ for data, desired_result in verification_data:
     if desired_result == np.argmax(last_layer_activation):
         result_counter += 1
 
-print(f"{result_counter} of {len(test_X)} test images were verified correctly by my net work.")
+sum_counter = sum([result == np.argmax(neural_network.feed_forward(data)) for data, result in verification_data])
+
+print(f"{result_counter}, {sum_counter} of {len(verification_data)} test images were verified correctly by my net work.")
 
 reference_neural_network = nn.Network(layer_sizes, weights, [snn.convert_array(bias_vec) for bias_vec in biases])
 
---
+++ loadMnistData.py
@@ -0,0 +1,48 @@
+from keras.datasets import mnist
+import numpy as np
+from typing import Tuple
+
+
+def convert_number(number: int) -> np.ndarray:
+    """
+    This function converts a number of the training data of the expected results to a numpy array with a one at the
+    index num.
+
+    :return: Numpy array with a 1 at the index num.
+    """
+    vec = np.zeros(10)
+    vec[number] = 1.
+    return vec
+
+
+def load_data() -> Tuple[int, list, list]:
+    """
+    Loads the training data and verification data of the MNIST library. The images of the MNIST libraries are
+    represented by 1D numpy arrays with 8 Bit values representing the gray scale of the pixels in the images.
+
+
+    :return: Returns the number of pixels in the images, a list of tuples of numpy arrays containing the training inputs
+        and the corresponding correct output and a list of tuples of lists containing the verification input
+        (numpy array) and an int representing the number described by the corresponding numpy array.
+    """
+    num_pixels = 784  # Number  of pixels in a image.
+
+    # train_inputs = List of the gray scale (8 Bit) of a quadratic image with 784 pixels.
+    # desired_result = List of the numbers that the images in train_input are representing.
+    # test_inputs = List of the gray scale (8 Bit) of a quadratic image with 784 pixels.
+    # test_results = List of numbers that the images in test_inputs are representing.
+    (train_inputs, desired_results), (test_inputs, test_results) = mnist.load_data()  # Load the test data
+
+    # Convert the elements of the desired results from numbers to numpy arrays.
+    converted_desired_results = np.array([convert_number(num) for num in desired_results])
+    # Reshape the training input so that each entry is a 1D vector.
+    train_inputs = train_inputs.reshape(len(train_inputs), num_pixels)
+    # Make the training data a list of tuples of numpy arrays.
+    training_data = list(zip(train_inputs, converted_desired_results))
+
+    # Reshape the test inputs so that the entries are 1D instead of 2D.
+    test_inputs = test_inputs.reshape(len(test_inputs), num_pixels)
+    # Create a list of tuples with the input and the corresponding result
+    verification_data = list(zip(test_inputs, test_results))
+
+    return num_pixels, training_data, verification_data
