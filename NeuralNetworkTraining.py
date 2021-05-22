0c92217a960a3c6142f9d0090c505c90834d33b0
Tobias Liebmann <tobias.liebmann@gmx.de>
2021-05-23 01:38:11 +0200

    REfactored a little bit of code of the training of the neural network.

--- NeuralNetworkTraining.py
+++ NeuralNetworkTraining.py
@@ -2,6 +2,7 @@ import numpy as np
 import SimpleNeuralNetwork as snn
 import packages.NeuralNetworkPython3.chapter1_2.Network as nn
 import loadMnistData as lmd
+import time as tm
 
 
 # -------------------
@@ -12,7 +13,7 @@ NUM_PIXELS, TRAINING_DATA, VERIFICATION_DATA = lmd.load_data()  # Load the MNIST
 
 MINI_BATCH_SIZE = 10  # Size of the mini batches used in the stochastic gradient descent.
 LEARNING_RATE = 3.  # Learning rate often declared by an eta.
-EPOCHS = 1  # Number of epochs used in the stochastic gradient descent.
+EPOCHS = 2  # Number of epochs used in the stochastic gradient descent.
 NUM_OUTPUT_NEURONS = 10  # Number of neurons in the output layer.
 NUM_HIDDEN_LAYER_NEURONS = 30  # Number of neurons in a hidden layer.
 # Sizes of the layers in the neural network.
@@ -32,8 +33,11 @@ neural_network = snn.SimpleNeuralNetwork(LAYER_SIZES, weights, biases)  # Define
 # Train the network and view results
 # ----------------------------------
 print("Started learning.")
-neural_network.learn(TRAINING_DATA, MINI_BATCH_SIZE, EPOCHS, LEARNING_RATE, shuffle_flag=False, verification_data=VERIFICATION_DATA)  # Let the neural network learn.
-print("Finished learning.")
+start = tm.time()
+neural_network.learn(TRAINING_DATA, MINI_BATCH_SIZE, EPOCHS, LEARNING_RATE, shuffle_flag=False,
+                     verification_data=VERIFICATION_DATA)  # Let the neural network learn.
+end = tm.time()
+print(f"Finished learning. My network needed: {end - start} s.")
 
 # -------------------------------------
 # Setup and train the reference network
@@ -43,13 +47,13 @@ print("Finished learning.")
 biases = [snn.convert_array(bias_vec) for bias_vec in biases]
 reference_neural_network = nn.Network(LAYER_SIZES, weights, biases)
 
-# for old_b, new_b in zip(old_biases, new_biases):
-#    print(old_b, "\n", new_b, "\n\n")
-
 # Convert data to the format Michael Nielsen uses.
 TRAINING_DATA = [(snn.convert_array(x), snn.convert_array(y)) for x, y in TRAINING_DATA]
 VERIFICATION_DATA = [(snn.convert_array(x), y) for x, y in VERIFICATION_DATA]
 
 # Train the network.
+start = tm.time()
 reference_neural_network.learn(TRAINING_DATA, EPOCHS, MINI_BATCH_SIZE, LEARNING_RATE, test_data=VERIFICATION_DATA,
                                shuffle_flag=False)
+end = tm.time()
+print(f"Michael Nielsen's network needed: {end - start} s.")
